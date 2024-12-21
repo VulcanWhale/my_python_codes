675141bd8610b719379bf8938e63dd89be78bae6
VulcanWhale <dasguptaanik462@gmail.com>
2024-12-21 18:17:34 +0530

    commit for the armstrong range with function.

--- pr20.py
+++ pr20.py
@@ -1,17 +1,21 @@
 # program to print the armstrong numbers in a range
 
-l = int(input("Please enter the lower limit: "))
-u = int(input("Please enter the upper limit: "))
 
-for n in range(l,u+1):
-    l = len(str(n))
-    sum = 0
-    n2 = n
+def armrange(l,u):
+    for n in range(l,u+1):
+        le = len(str(n))
+        sum = 0
+        n2 = n
 
-    while n2>0:
-        d = n2 % 10
-        sum += d ** l
-        n2 //= 10
-    if n == sum:
-        print(n)
-        
+        while n2>0:
+            d = n2 % 10
+            sum += d ** le
+            n2 //= 10
+
+        if n == sum:
+            print(n)
+
+lower = int(input("Please enter the lower limit: "))
+upper = int(input("Please enter the upper limit: "))
+
+armrange(lower,upper)
