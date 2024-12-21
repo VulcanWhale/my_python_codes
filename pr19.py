2b8bca4f731dc6d938b01a6b15c862250d313c5c
VulcanWhale <dasguptaanik462@gmail.com>
2024-12-21 16:31:04 +0530

    casual commit

--- pr19.py
+++ pr19.py
@@ -2,14 +2,17 @@
 """An Armstrong number (also known as a narcissistic number or pluperfect digital invariant) 
 is a number that is equal to the sum of its own digits each raised to the power of the number of digits."""
 
-n = int(input("Please enter a number: "))
-
 def armstrong(n):
     t_sum = 0
     l = len(str(n))
 
-    t_sum = sum(n ** l for i in n)
+    t_sum = sum(n ** l for i in str(n))
 
     return t_sum == n
 
+n = int(input("Please enter a number: "))
 
+if armstrong(n):
+    print(f"{n} is an armstrong number.")
+else:
+    print(f"{n} is not an armstrong number.")
