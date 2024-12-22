6a1145030e082a9f6701935cfa90c8109f1b90a5
VulcanWhale <dasguptaanik462@gmail.com>
2024-12-22 19:57:09 +0530

    this is the final commit.

--- pr22.py
+++ pr22.py
@@ -1,9 +1,12 @@
 # python program to Display the powers of 2 using anonymous function
 # i will use lambda function iin this program
 
+def power(t):
+    res = list(map(lambda x: 2**x, range(t)))
+
+    for i in range(t):
+        print("2 to the power of ", i, "is", res[i])
+
+
 t = int(input("Pleasse enter the number of terms you want: "))
-
-res = list(map(lambda x: 2**x, range(t)))
-
-for i in range(t):
-    print("2 to the power of ", i, "is", res[i])
+power(t)
