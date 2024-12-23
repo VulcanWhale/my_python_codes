9d7ce8421fb0a53f507e00d0e7031d2d31997237
VulcanWhale <dasguptaanik462@gmail.com>
2024-12-23 16:32:34 +0530

    converted the number to binary, lambda, hexadecimal

---
+++ pr24.py
@@ -0,0 +1,8 @@
+#Python Program to Convert Decimal to Binary, Octal and Hexadecimal
+
+n = int(input("Please enter a number: "))
+
+binary = lambda x: bin(x)
+octal = lambda x: oct(x)
+hexadecimal = lambda x: hex(x)
+
