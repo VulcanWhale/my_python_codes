8720748e5f752647e141dfa1a3224f961e6e1f5c
VulcanWhale <dasguptaanik462@gmail.com>
2024-12-23 16:51:46 +0530

    printing the converted number to binary, lambda, hexadecimal

--- pr24.py
+++ pr24.py
@@ -6,3 +6,7 @@ binary = lambda x: bin(x)
 octal = lambda x: oct(x)
 hexadecimal = lambda x: hex(x)
 
+print(f"The decimal value of {n} is:")
+print(f"{binary(n)} in binary.")
+print(f"{octal(n)} in octal.")
+print(f"{hexadecimal(n)} in Hexadecimal.")
