b988f83d9011fee78d67c6be9ceaaef2486705a4
VulcanWhale <dasguptaanik462@gmail.com>
2024-12-27 23:44:42 +0530

    optimised the loop

--- pr28.py
+++ pr28.py
@@ -1,8 +1,9 @@
 # program to find the factor of a number
 
 n = int(input("Please enter a number: "))
-i = 2
+i = 1
 while i <= n/2:
     if n % i == 0:
         print(f"{i} is a factor of {n}")
     i += 1
+print(f"{n} is a factor of {n} itself.")
