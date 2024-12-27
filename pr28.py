b1026dbdea44bf83d9459f18f0f4ddcbc7d0f2f2
VulcanWhale <dasguptaanik462@gmail.com>
2024-12-27 23:39:32 +0530

    made i = 2

--- pr28.py
+++ pr28.py
@@ -1,9 +1,8 @@
 # program to find the factor of a number
 
 n = int(input("Please enter a number: "))
-i = 1
+i = 2
 while i <= n/2:
     if n % i == 0:
         print(f"{i} is a factor of {n}")
     i += 1
-    
