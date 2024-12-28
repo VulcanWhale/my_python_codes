170d9a75bc71fed8d1eeee60ac7220fd800c07ad
VulcanWhale <dasguptaanik462@gmail.com>
2024-12-28 15:28:59 +0530

    writing the logic for while loop

--- pr29.py
+++ pr29.py
@@ -1,10 +1,17 @@
 # making a simple calculator application in python
 
-def add(a,b):
-    return lambda: a+b
-
-def sub(a,b):
-    return lambda: a-b
+def inp():
+    a = float(input('Enter the first number: '))
+    b = float(input('Enter the second number: '))
+    return a,b
 
 
 print("This is a simple calculator. \nPlease choose an option given below: \n1>ADD\n2>SUBTRACT\n3>MULTIPLICATION\n4>DIVISION\n5>EXIT")
+c = int(input("Enter your option: "))
+
+while True:
+    if c == 5:
+        print("Thank you!")
+        break
+
+    a,b = inp()
