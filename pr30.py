8f13b3d501da768cdb48cc005d1a7c070af726be
VulcanWhale <dasguptaanik462@gmail.com>
2024-12-28 19:35:45 +0530

    cards printing

--- pr30.py
+++ pr30.py
@@ -10,3 +10,9 @@ deck = [(r,s) for r in ranks for s in suits]
 
 # using random module to shuffle the cards
 random.shuffle(deck)
+
+# no we need to print the first five cards from the shuffled deck
+
+print("The cards you got are as follows: ")
+for i in range(5):
+    print(f"{deck[i][0]} of {deck[i][1]}")
