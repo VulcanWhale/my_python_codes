94a9938a0ca81221b88b76f4cd2f180756a325d3
VulcanWhale <dasguptaanik462@gmail.com>
2024-12-28 14:13:48 +0530

    Printing the options

---
+++ pr29.py
@@ -0,0 +1,10 @@
+# making a simple calculator application in python
+
+def add(a,b):
+    return lambda: a+b
+
+def sub(a,b):
+    return lambda: a-b
+
+
+print("This is a simple calculator. \nPlease choose an option given below: \n1>ADD\n2>SUBTRACT\n3>MULTIPLICATION\n4>DIVISION\n5>EXIT")
