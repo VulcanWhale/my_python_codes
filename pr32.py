ba449199853a7774c02546873b3e7d3e2016467f
VulcanWhale <dasguptaanik462@gmail.com>
2024-12-30 12:43:08 +0530

    logic for recursion

---
+++ pr32.py
@@ -0,0 +1,9 @@
+#python program to display the fibonacci series recursively
+
+def fibo(n):
+    if n <= 1:
+        return n
+    else:
+        return fibo(n-1) + fibo(n-2)
+    
+num = int(input("Please enter a number: "))
