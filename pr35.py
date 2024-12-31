f2212334235a043633e61f9353593fd7c0e388b8
VulcanWhale <dasguptaanik462@gmail.com>
2024-12-31 14:04:47 +0530

    writing the rwcursion function

---
+++ pr35.py
@@ -0,0 +1,8 @@
+# program to convert decimal number to binary number using recursion
+
+def bin(num):
+    if num > 1:
+        bin(num // 2)
+    print(n % 2, end = ' ')
+
+n = int(input("Please enter the decimal number: "))
