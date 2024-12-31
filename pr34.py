59be16a6b0a897bbf2c21fd57c139e6ed7d68394
VulcanWhale <dasguptaanik462@gmail.com>
2024-12-31 13:42:38 +0530

    input coddition checked

--- pr34.py
+++ pr34.py
@@ -4,4 +4,13 @@ def rec_fac(n):
     if n == 1:
         return n
     else:
-        return 
+        return rec_fac(n-1) + rec_fac(n-2)
+    
+num = int(input("Please enter a number: "))
+
+if num < 0:
+    print("factorial is not possible for negative numbers.")
+elif num == 0:
+    print("The factorial of 0 is 1")
+else:
+    print(f'The factorial of {num} is', rec_fac(num))
