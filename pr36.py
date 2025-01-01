3f92cc2cb853e8f0504b7d6153fa2e70972ee965
VulcanWhale <dasguptaanik462@gmail.com>
2025-01-01 20:07:35 +0530

    taking the input from the user

--- pr36.py
+++ pr36.py
@@ -2,20 +2,22 @@
 
 # defining the matrices
 
-a = [[2,3,5],
-     [4,27,12],
-     [23,44,63]]
+import ast
+a = ast.literal_eval(input("Please enter the first matrix as nested list (3x3): "))
 
-b = [[7,4,2],
-     [34,33,11],
-     [13,23,19]]
+b = ast.literal_eval(input("Please enter the second matrix as nested list (3x3): "))
+
+
+#a = [[2,3,5],[4,27,12],[23,44,63]]
+
+#b = [[7,4,2],[34,33,11],[13,23,19]]
 
 res = [[0,0,0],
        [0,0,0],
        [0,0,0]]
 
 
-for i in (len(a)):
+for i in range(len(a)):
     for j in range(len(a[0])):
         res[i][j] = a[i][j] + b[i][j]
 
