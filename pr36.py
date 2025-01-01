00ad6a329d698b6c7e412a51a7157d13e3a25cf5
VulcanWhale <dasguptaanik462@gmail.com>
2025-01-01 19:56:53 +0530

    wrote the function for matrix addition

---
+++ pr36.py
@@ -0,0 +1,18 @@
+# program to add two matrices
+
+# defining the matrices
+
+a = [[2,3,5],
+     [4,27,12],
+     [23,44,63]]
+
+b = [[7,4,2],
+     [34,33,11],
+     [13,23,19]]
+
+res = [[0,0,0],
+       [0,0,0],
+       [0,0,0]]
+
+
+for i in (len(x))
